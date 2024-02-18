4253d150b07e3a9895e5f750561aa6be1d939c6d
Sara Justino <sara.sjustino@gmail.com>
2024-02-17 22:19:57 -0300

    Primeiro commit

---
+++ projeto1/resources/views/eu.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sara Justino</title>
+</head>
+<body>
+
+<img src="eu.webp" alt="uma foto minha">
+
+</body>
+</html>
